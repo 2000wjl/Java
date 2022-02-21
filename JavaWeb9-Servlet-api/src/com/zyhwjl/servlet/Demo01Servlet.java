e2c4f26ea59d40b0ab08e5ab9e9b23f3b89a38db
zyhwj <94264zyh>
2022-02-21 10:33:23 +0800

    new Servlet-api

---
+++ JavaWeb9-Servlet-api/src/com/zyhwjl/servlet/Demo01Servlet.java
@@ -0,0 +1,12 @@
+package com.zyhwjl.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @Description :
+ * @Author : ZYHWJL E-mail:zyhwjl@zyhwjl.cn
+ * @Date : 2022/2/21 9:59
+ */
+public class Demo01Servlet extends HttpServlet {
+
+}
